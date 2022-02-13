60d4831f3b8e61bb37d068b0cfaf6f38f890245d
alexheil <aheil4250@gmail.com>
2022-02-13 15:34:36 -0500

    1

--- app/uploaders/logo_uploader.rb
+++
@@ -1,7 +0,0 @@
-class LogoUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-
-  version :thumb do
-    process resize_to_fill: [156,156]
-  end
-end
--- app/uploaders/u_block_image_uploader.rb
+++
@@ -1,5 +0,0 @@
-class UBlockImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [1500, 1500]
-end
