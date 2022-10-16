5ddd6b0021e90a1ef7472ea4c9f5d051c5d83636
alexheil <aheil4250@gmail.com>
2022-10-15 21:48:37 -0400

    speed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,36 +11,10 @@ var flashTimeout = function() {
   $("img").fadeIn(1000);
   $("img").css("display", "block");
 
-  $("a.add_fields").data("association-insertion-method", 'before').data("association-insertion-node", '#u_blocks_target');
-
 };
 
 $(document).ready(flashTimeout);
 $(document).on('turbolinks:load', flashTimeout);
-
-
-var menuToggle = function() {
-
-  $('#menu-click-open').click(function() {
-    $('#nd-menu-container').fadeIn(300);
-  });
-
-  $('#menu-click-close').click(function() {
-    $('#nd-menu-container').fadeOut(300);
-  });
-
-  $('.js-popup-click').click(function() {
-    $('.js-popup-container').fadeIn(300);
-  });
-
-  $('.js-popup-click-close').click(function() {
-    $('.js-popup-container').fadeOut(300);
-  });
-
-};
-
-$(document).ready(menuToggle);
-$(document).on('turbolinks:load', menuToggle);
 
 
 var formTabs = function() {
